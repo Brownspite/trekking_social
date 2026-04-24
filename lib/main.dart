8d3c9cd9ac4cfb793e15b11501611cc19af2279e
brownspite <mohtasimahmedsami@gmail.com>
2026-04-24 23:39:01 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
+import 'services/event_service.dart';
 import 'screens/splash_screen.dart';
 import 'screens/app_shell.dart';
 
@@ -10,6 +11,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await EventService().seedEvents();
   runApp(const MyApp());
 }
 
